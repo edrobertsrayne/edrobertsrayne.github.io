e4b8a73bd56b0e498808119c74f8ef93e35e6d4a
Ed Roberts Rayne <ed.rayne@gmail.com>
2024-02-25 13:30:57 +0000

    Added Catpuccin colour themes

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,7 +10,37 @@ module.exports = {
   },
   plugins: [require("@tailwindcss/typography"), require("daisyui")],
   daisyui: {
-    themes: ["cupcake", "synthwave"],
+    themes: [
+      {
+        light: {
+          primary: "#8839ef",
+          secondary: "04a5e5",
+          accent: "#ea76cb",
+          neutral: "#7c7f93",
+          "base-100": "#eff1f5",
+          "base-200": "#e6e9ef",
+          "base-300": "#dce0e8",
+          info: "#1e66f5",
+          success: "#40a02b",
+          warning: "#df8e1d",
+          error: "#d20f39",
+        },
+      },
+      {
+        dark: {
+          primary: "#cba6f7",
+          secondary: "#89dceb",
+          accent: "#f5c2ef",
+          neutral: "#313244",
+          "neutral-content": "#000000",
+          "accent-content": "#11111b",
+          "base-100": "#1e1e2e",
+          info: "#89b4fa",
+          success: "#a6e3a1",
+          warning: "#f9e2af",
+          error: "#f38ba8",
+        },
+      },
+    ],
   },
 };
-
