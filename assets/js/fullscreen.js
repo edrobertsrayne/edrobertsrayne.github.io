eb4f01abd2964a7390eb98f57dc3dbcf837aa649
Ed Roberts Rayne <ed.rayne@gmail.com>
2024-02-26 21:53:16 +0000

    iFrame embedding of sketches using sketch shortcode
    Use {{< sketch src >}} to embed sketches with a fullscreen button. Sketches either hosted externally or in the static folder.

---
+++ assets/js/fullscreen.js
@@ -0,0 +1,16 @@
+function toggleFullScreen(elementId) {
+  var element = document.getElementById(elementId);
+  console.log(element);
+  if (element.requestFullscreen) {
+    element.requestFullscreen();
+  } else if (element.mozRequestFullScreen) {
+    /* Firefox */
+    element.mozRequestFullScreen();
+  } else if (element.webkitRequestFullscreen) {
+    /* Chrome, Safari & Opera */
+    element.webkitRequestFullscreen();
+  } else if (element.msRequestFullscreen) {
+    /* IE/Edge */
+    element.msRequestFullscreen();
+  }
+}
--- content/posts/test/index.md
+++ content/posts/test/index.md
@@ -5,12 +5,8 @@ date = 2024-02-22T14:15:05Z
 draft = true
 categories = ['coding']
 tags = ['javascript', 'arduino']
-p5js = true
-
-[[resources]]
-src = 'sketch.js'
 +++
 
-This is a test post. I have no idea how well it will work.
+{{< sketch "/starfield/index.html" >}}
 
-## Hello
+This is a test post. I have no idea how well it will work.
